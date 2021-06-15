2402b9029e525328e01e28f1fcc26914746d4653
vmanjunath13 <vmanjunath@hawk.iit.edu>
2021-06-15 08:35:39 -0500

    Controller implementation

--- src/main/java/com/egen/controller/OrderController.java
+++ src/main/java/com/egen/controller/OrderController.java
@@ -1,8 +1,12 @@
 package com.egen.controller;
 
 import com.egen.model.Order;
+import com.egen.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.ZonedDateTime;
@@ -15,25 +19,30 @@ public class OrderController {
      * implement the following endpoints
      */
 
+    OrderRepository orderRepository;
+
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
     @GetMapping("order")
-    public ResponseEntity<List<Order>> getAllOrders(){
-        //TODO
-        return ResponseEntity.ok(Collections.singletonList(new Order("id")));
+    public ResponseEntity<List<Order>> getAllOrders() {
+        return ResponseEntity.ok(this.orderRepository.getOrders());
     }
 
-    public ResponseEntity<List<Order>> getOrderById(String id){
-        //TODO
-        return null;
+    @GetMapping("order/{oid}")
+    public ResponseEntity<Order> getOrderById(@PathVariable(name = "oid") String id) {
+        return ResponseEntity.ok(this.orderRepository.getOrderById(id));
     }
 
-    public ResponseEntity<List<Order>> getAllOrdersWithInInterval(ZonedDateTime startTime, ZonedDateTime endTime){
-        //TODO
-        return null;
+    @GetMapping(value = "order", params = { "startTime", "endTime" })
+    public ResponseEntity<List<Order>> getAllOrdersWithInInterval(@RequestParam("startTime") ZonedDateTime startTime, @RequestParam("endTime") ZonedDateTime endTime){
+        return ResponseEntity.ok(this.orderRepository.getOrdersWithTimeInterval(startTime.toString(), endTime.toString()));
     }
 
-    public ResponseEntity<List<Order>> top10OrdersWithHighestDollarAmountInZip(String zip){
-        //TODO
-        return null;
+    @GetMapping(value = "order", params = {"zip"})
+    public ResponseEntity<List<Order>> top10OrdersWithHighestDollarAmountInZip(@RequestParam("zip") String zip) {
+        return ResponseEntity.ok(this.orderRepository.getOrders());
     }
 
     public ResponseEntity<Order> placeOrder(Order order){
